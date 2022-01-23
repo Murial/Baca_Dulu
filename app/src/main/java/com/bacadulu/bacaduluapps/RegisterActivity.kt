dcf0b1f4423e0e904226b710e2efc025fd49f117
Dwi Gunawan <dwi19ti@mahasiswa.pcr.ac.id>
2022-01-24 00:36:21 +0700

    Registrasi Fix
    Berhasil Melakukan Registrasi dengan Smartphone tetapi emulator gagal registrasi karena masalah jaringan

--- app/src/main/java/com/bacadulu/bacaduluapps/RegisterActivity.kt
+++ app/src/main/java/com/bacadulu/bacaduluapps/RegisterActivity.kt
@@ -3,19 +3,66 @@ package com.bacadulu.bacaduluapps
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.bacadulu.bacaduluapps.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         val binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnSignUp.setOnClickListener {
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+
+            if (email.isEmpty()){
+                binding.etEmail.error = "Email Harus Diisi"
+                binding.etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.etEmail.error = "Email Tidak Valid"
+                binding.etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty() || password.length < 6){
+                binding.etPassword.error = "Password harus lebih dari 6 karakter"
+                binding.etPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            registerUser(email, password)
+        }
+
         binding.btnLogin.setOnClickListener {
             Intent(this@RegisterActivity, LoginActivity::class.java).also {
                 startActivity(it)
             }
         }
+    }
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if(it.isSuccessful){
+                    Intent(this@RegisterActivity, HomeActivity::class.java).also {
+                        it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(it)
+                    }
+                } else {
+                    Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
