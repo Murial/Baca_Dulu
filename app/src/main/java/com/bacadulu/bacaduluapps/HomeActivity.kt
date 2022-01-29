abc6f120dad89168ff5b6af2cc6081e217ce7790
Dwi Gunawan <dwi19ti@mahasiswa.pcr.ac.id>
2022-01-29 21:54:10 +0700

    Login dan Logout
    Login dan Logout berhasil dilakukan, Penambahan permission pada manifest juga dilakukan untuk mengatasi permasalahan jaringan pada emulator

--- app/src/main/java/com/bacadulu/bacaduluapps/HomeActivity.kt
+++ app/src/main/java/com/bacadulu/bacaduluapps/HomeActivity.kt
@@ -1,11 +1,30 @@
 package com.bacadulu.bacaduluapps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bacadulu.bacaduluapps.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        auth = FirebaseAuth.getInstance()
+
+        val binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogout.setOnClickListener{
+            auth.signOut()
+            Intent(this@HomeActivity, LoginActivity::class.java).also {
+                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(it)
+            }
+        }
     }
 }
--- app/src/main/java/com/bacadulu/bacaduluapps/LoginActivity.kt
+++ app/src/main/java/com/bacadulu/bacaduluapps/LoginActivity.kt
@@ -3,17 +3,74 @@ package com.bacadulu.bacaduluapps
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.bacadulu.bacaduluapps.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+
+            if (email.isEmpty()){
+                binding.etEmail.error = "Email Harus Diisi"
+                binding.etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.etEmail.error = "Email Tidak Valid"
+                binding.etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty() || password.length < 6){
+                binding.etPassword.error = "Password harus lebih dari 6 karakter"
+                binding.etPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            LoginUser(email,password)
+        }
+
         binding.btnRegister.setOnClickListener {
             Intent(this@LoginActivity, RegisterActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+
+    private fun LoginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Intent(this@LoginActivity,HomeActivity::class.java).also{
+                        it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(it)
+                    }
+                } else {
+                    Toast.makeText(this,"${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (auth.currentUser!=null){
+            Intent(this@LoginActivity,HomeActivity::class.java).also{
+                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(it)
             }
         }
