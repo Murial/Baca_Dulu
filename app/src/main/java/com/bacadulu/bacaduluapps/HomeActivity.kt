4b0046281e9fdc4b539ad268fa4bf2b0e7f78d5e
Dwi Gunawan <dwi19ti@mahasiswa.pcr.ac.id>
2022-02-06 20:31:04 +0700

    Membuat Bottom Navbar dan Splash Screen
    Bottom Navbar berisi fragment home, favorit, history dan logout, sudah selesai dibuat dan dapat digunakan tanpa error. Splash Screen berhasil dibuat yang mana itu adalah tampilan pertama saat program baru dijalankan.

--- app/src/main/java/com/bacadulu/bacaduluapps/HomeActivity.kt
+++ app/src/main/java/com/bacadulu/bacaduluapps/HomeActivity.kt
@@ -3,7 +3,11 @@ package com.bacadulu.bacaduluapps
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.bacadulu.bacaduluapps.databinding.ActivityHomeBinding
+import com.bacadulu.bacaduluapps.fragments.FavoriteFragment
+import com.bacadulu.bacaduluapps.fragments.HistoryFragment
+import com.bacadulu.bacaduluapps.fragments.HomeFragment
 import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
@@ -19,12 +23,35 @@ class HomeActivity : AppCompatActivity() {
         val binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLogout.setOnClickListener{
-            auth.signOut()
-            Intent(this@HomeActivity, LoginActivity::class.java).also {
-                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                startActivity(it)
+        val HomeFragment = HomeFragment()
+        val FavoriteFragment = FavoriteFragment()
+        val HistoryFragment = HistoryFragment()
+
+        makeCurrentFragment(HomeFragment)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home -> makeCurrentFragment(HomeFragment)
+                R.id.ic_favorite -> makeCurrentFragment(FavoriteFragment)
+                R.id.ic_history -> makeCurrentFragment(HistoryFragment)
+                R.id.ic_logout -> btnLogoutClicked()
             }
+            true
+        }
+    }
+
+    private fun btnLogoutClicked() {
+        auth.signOut()
+        Intent(this@HomeActivity, LoginActivity::class.java).also {
+            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(it)
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
         }
     }
 }
